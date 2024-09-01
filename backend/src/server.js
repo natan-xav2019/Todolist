20a84eb0232e3a462095aad0e60ce462902b7d62
Natan Xavier <natanxaviersiquera@gmail.com>
2024-09-01 15:46:52 -0300

    Refactor: Renomeando arquivo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
-const express = require("express")
-const app = express()
-const porta = 3333 
+const app = require("./app")
+const porta = 3333
 
 app.listen(porta, () => console.log(`Servidor rodando na porta http://localhost:${porta}/`))
