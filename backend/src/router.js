aab150dadda1f3fc14e98ac884f0291d2f89e2e3
Natan Xavier <natanxaviersiquera@gmail.com>
2024-09-01 17:19:30 -0300

    Feat: implementando tasks getAll().
    Do model e passado  para o controller e passa para o router a função getAll() retornando todas as tasks do banco de dados e mostrando na rota get tasks

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,7 @@
 const express = require('express')
-
+const tasksController = require('./controllers/tasksControlles')
 const router = express.Router()
+
+router.get('/tasks', tasksController.getAll)
 
 module.exports = router
