d6567a04bf719ed02af9223a9aaf0b3cb5e619c8
Natan Xavier <natanxaviersiquera@gmail.com>
2024-09-08 11:27:34 -0300

    Feat(router): updateTask

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -20,10 +20,19 @@ const deleteTask = async (id) => {
     const query = 'DELETE FROM tasks WHERE id = ?'
     const removedTask = await connection.execute(query, [id])
     return removedTask
-} 
+}
+
+const updateTask = async (id, task) => {
+    const { title, status } = task
+    
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?'
+    const updatedTask = await connection.execute(query, [title,status,id])
+    return updatedTask
+}
 
 module.exports = {
     getAll,
     createTask,
     deleteTask,
+    updateTask,
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -6,5 +6,6 @@ const router = express.Router()
 router.get('/tasks', tasksController.getAll)
 router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask)
 router.delete('/tasks/:id',tasksController.deleteTask)
+router.put('/tasks/:id',tasksController.updateTask)
 
 module.exports = router
