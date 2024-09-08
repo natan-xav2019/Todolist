f280643422973710c659e1270efe579a0b7e9231
Natan Xavier <natanxaviersiquera@gmail.com>
2024-09-08 09:05:07 -0300

    Feat(router): Post createTask

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,10 +1,22 @@
 const connection = require(`./connection`)
 
 const getAll = async () => {
-    const tasks = await connection.execute('SELECT * FROM tasks')
+    const [tasks] = await connection.execute('SELECT * FROM tasks')
     return tasks
 }
 
+const createTask = async (tasks) => {
+    const { title } = tasks;
+    const dateUTC = new Date(Date.now()).toUTCString()
+
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUE(?, ?, ?)'
+
+    const [createdTask] = await connection.execute(query,[title,"pendente",dateUTC])
+
+    return {insertId: createdTask.insertId}
+} 
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -3,5 +3,6 @@ const tasksController = require('./controllers/tasksControlles')
 const router = express.Router()
 
 router.get('/tasks', tasksController.getAll)
+router.post('/tasks', tasksController.createTask)
 
 module.exports = router
