1812378f11dbdf70fa5aa5fd00676764f0cd72b3
Natan Xavier <natanxaviersiquera@gmail.com>
2024-10-10 17:28:22 -0300

    Feat: função createRow
    criando as seguintes funções:
    - createElement(tag, innerTextinnerHTML)
    - createButton(innerText)
    - createSelect(task)
    - createOption(values,value)
    
    Essas funções são usadas na criação de novas rows assim facilitando futuras atualizações se necessário.

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,95 @@
-const url = 'http://localhost:3333/tasks' 
+const url = 'http://localhost:3333/tasks'
+
+const tbody = document.querySelector("tbody")
 
 const fetchTasks = async () => {
     const response = await fetch(url)
     const tasks = await response.json()
     return tasks
 }
+
+const createElement = (tag, innerText = '', innerHTML = '') => {
+    const element = document.createElement(tag)
+
+    if(innerText) 
+        element.innerText = innerText
+
+    if(innerHTML) 
+        element.innerHTML = innerHTML
+
+    return element
+}
+
+const createButton = (innerText = '') => {
+    const btn = document.createElement('button')
+    const span = document.createElement('span')
+
+    btn.classList.add("btn-action")
+
+    span.innerText = innerText
+    span.classList.add("material-symbols-outlined")
+
+    btn.appendChild(span)
+
+    return btn
+}
+
+const createSelect = (values = [""],value = "") => {
+    const select = document.createElement('select')
+    const options = values.map(createOption)
+
+    options.forEach(option => {
+        select.appendChild(option)
+    })
+
+    if(value)
+        select.value = value
+
+    return select    
+}
+
+const createOption = (value = '') => {
+    option = createElement("option", value)
+    option.value = value
+
+    return option
+}
+
+const createRow = (task) => {
+
+    const {id ,title, created_at, status} = task
+    const options = ['pendente', 'em andamento', 'concluido']
+
+    const tr = createElement('tr')
+    const tdTitle = createElement('td', title)
+    const tdCreatedAt = createElement('td', created_at)
+    const tdStatus = createElement('td')
+    const tdActions = createElement('td')
+
+    const select = createSelect(options,status)
+    tdStatus.appendChild(select)
+
+    const editButton = createButton("edit")
+    const deleteButton = createButton("delete")
+
+    tdActions.appendChild(editButton)
+    tdActions.appendChild(deleteButton)
+
+    tr.appendChild(tdTitle)
+    tr.appendChild(tdCreatedAt)
+    tr.appendChild(tdStatus)
+    tr.appendChild(tdActions)
+
+    return tr
+}
+
+const loadTasks = async () => {
+    const tasks = await fetchTasks()
+    
+    tasks.forEach(task => {
+        const tr = createRow(task)
+        tbody.appendChild(tr)
+    })
+}
+
+loadTasks()
